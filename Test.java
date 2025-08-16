da896ff9f496b045c88ed4ad751e4ad0e5e35ce2
saniya-tech786 <saniya.shaikh.web@gmail.com>
2025-08-16 13:37:38 +0530

    Day 1 practice 16 Aug

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+        String name=" Saniya Begum ";
+        String uppercase=name.toUpperCase();
+        System.out.println(uppercase);
+
+        String lowercase=name.toLowerCase();
+        System.out.println(lowercase);
+
+        String trim=name.trim();
+        System.out.println(trim);
+
+        String replace=name.replace('a','@');
+        System.out.println(replace);
+
+        String replace_word=name.replace("Begum","IAS");
+        System.out.println(replace_word);
+
+    }
+
+
+
+        
+    
+}
