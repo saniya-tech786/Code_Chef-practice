383400709a6254c945f9c6f127698af9231b5320
Saniya Shaikh <saniya.shaikh.web@gmail.com>
2025-08-30 19:00:10 +0530

    Add files via upload
    array practice

---
+++ Array1.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Array1 {
+    public static void main(String[] args) {
+        ArrayList<Integer>number;
+        System.out.println("An ArrayList declared");
+    }
+    
+}
---
+++ Array2.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Array2 {
+    public static void main(String[] args) {
+        ArrayList<String>names;
+        System.out.println("this is a String type of array list");
+    }
+    
+}
